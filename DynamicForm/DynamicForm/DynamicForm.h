16fe6a781fe0c245b2e779c0d09f4a276bbeeee3
Deepak Tagadiya <deepak.tagadiya@zealousys.local>
2022-04-28 10:51:24 +0530

    Added framework project.

---
+++ DynamicForm/DynamicForm/DynamicForm.h
@@ -0,0 +1,18 @@
+//
+//  DynamicForm.h
+//  DynamicForm
+//
+//  Created by Deepak Tagadiya on 28/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DynamicForm.
+FOUNDATION_EXPORT double DynamicFormVersionNumber;
+
+//! Project version string for DynamicForm.
+FOUNDATION_EXPORT const unsigned char DynamicFormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicForm/PublicHeader.h>
+
+
---
+++ DynamicForm/DynamicForm/NormalClassFile.swift
@@ -0,0 +1,14 @@
+//
+//  NormalClassFile.swift
+//  DynamicForm
+//
+//  Created by Deepak Tagadiya on 28/04/22.
+//
+
+import Foundation
+
+public struct DemoFunction {
+    public static func enterYourPrivateKey(str: String) {
+        print("Your private key is:", str)
+    }
+}
